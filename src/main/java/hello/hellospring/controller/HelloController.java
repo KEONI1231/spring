0287ec8d24c870ecc2e43ce6f77f642e80aefe8f
keoni <kimkeonhwi991231@gmail.com>
2023-10-31 07:41:35 +0900

    test case, user service

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
@@ -15,10 +15,37 @@ public class HelloController {
         //template에 있는 hello.html을 찾아라, 즉 html이름 바꾸면 get안됨
         //기본적으로 resources의 templates의 파일을 찾음
     }
-
     @GetMapping("hello-mvc")
-    public String helloMvc(@RequestParam("name") String name, Model model) {
+    public String helloMvc(@RequestParam(value = "name") String name, Model model) {
         model.addAttribute("name" , name);
         return "hello-template";
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody // http에서 헤더부와 바디부가 있는데 바디 부분에 리턴 값을 바로 넣어주겠다.
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; // ex) hello string
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        //System.out.println(hello.name);
+        return hello;
+    }
+    static class Hello
+    {
+        private String name;
+
+        //getter, setter : 자바 빈 규약
+        public String getName() {
+           return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
---
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,16 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public interface MemberRepository { //interface -> 부모 객체는 선언만 하며 정의(내용)은 자식에서 오버라이딩하여 사용해야함
+    //반면 extends는 부모에서 선언/ 정의를 모두하며 자식은 메소드 / 변수를 그대로 사용할 수 있음.
+    //abstract -> extends 와 interface의 혼합. extends하되 몇 개는 추상 메소드로 구현되어 있음.
+    Member save(Member member);
+    Optional<Member> findById(Long id); //옵셔녈?
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
